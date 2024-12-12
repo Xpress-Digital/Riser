c1fd91bf45b27c38c1166d0115c0b1ad9a5d3e1d
Lord-kristofa Ekerete <46141398+hortharn@users.noreply.github.com>
2024-12-12 14:42:11 +0100

    Main Upload Part 2

---
+++ lib/index.dart
@@ -0,0 +1,130 @@
+// Export pages
+export '/home/home_widget.dart' show HomeWidget;
+export '/play_affirmation/play_affirmation_widget.dart'
+    show PlayAffirmationWidget;
+export '/onboarding/onboarding_widget.dart' show OnboardingWidget;
+export '/onboarding1/onboarding1_widget.dart' show Onboarding1Widget;
+export '/sign_up/sign_up_widget.dart' show SignUpWidget;
+export '/forgot_password/forgot_password_widget.dart' show ForgotPasswordWidget;
+export '/onboarding_completed/onboarding_completed_widget.dart'
+    show OnboardingCompletedWidget;
+export '/testimonials/testimonials_widget.dart' show TestimonialsWidget;
+export '/main_profile/main_profile_widget.dart' show MainProfileWidget;
+export '/sign_in/sign_in_widget.dart' show SignInWidget;
+export '/welcome/welcome_widget.dart' show WelcomeWidget;
+export '/compleete_profile/compleete_profile_widget.dart'
+    show CompleeteProfileWidget;
+export '/kids_profile_setup1/kids_profile_setup1_widget.dart'
+    show KidsProfileSetup1Widget;
+export '/affirmations/affirmations_widget.dart' show AffirmationsWidget;
+export '/settings/settings_widget.dart' show SettingsWidget;
+export '/alarm/alarm_widget.dart' show AlarmWidget;
+export '/home02_list_copy/home02_list_copy_widget.dart'
+    show Home02ListCopyWidget;
+export '/kid_p_r_o_f_i_l_e/kid_p_r_o_f_i_l_e_widget.dart' show KidPROFILEWidget;
+export '/riser_affirmation/riser_affirmation_widget.dart'
+    show RiserAffirmationWidget;
+export '/affirmation_categories/affirmation_categories_widget.dart'
+    show AffirmationCategoriesWidget;
+export '/update_profile/update_profile_widget.dart' show UpdateProfileWidget;
+export '/favorite_affirmations/favorite_affirmations_widget.dart'
+    show FavoriteAffirmationsWidget;
+export '/profile_confirmationpage/profile_confirmationpage_widget.dart'
+    show ProfileConfirmationpageWidget;
+export '/edit_kids_profile/edit_kids_profile_widget.dart'
+    show EditKidsProfileWidget;
+export '/alarms/alarms_widget.dart' show AlarmsWidget;
+export '/subscriptionpage/subscriptionpage_widget.dart'
+    show SubscriptionpageWidget;
+export '/audio_player_identity/audio_player_identity_widget.dart'
+    show AudioPlayerIdentityWidget;
+export '/record_your_affirmation/record_your_affirmation_widget.dart'
+    show RecordYourAffirmationWidget;
+export '/feed_back/feed_back_widget.dart' show FeedBackWidget;
+export '/kids_list/kids_list_widget.dart' show KidsListWidget;
+export '/profile_confirmationpage2/profile_confirmationpage2_widget.dart'
+    show ProfileConfirmationpage2Widget;
+export '/profile_confirmationpage3/profile_confirmationpage3_widget.dart'
+    show ProfileConfirmationpage3Widget;
+export '/new_kids_profile/new_kids_profile_widget.dart'
+    show NewKidsProfileWidget;
+export '/interimhomepage/interimhomepage_widget.dart'
+    show InterimhomepageWidget;
+export '/music_player_2/music_player2_widget.dart' show MusicPlayer2Widget;
+export '/components/daily_affirmations_widget.dart'
+    show DailyAffirmationsWidget;
+export '/alarmpage2/alarmpage2_widget.dart' show Alarmpage2Widget;
+export '/sexualityand_gender_ideologies/sexualityand_gender_ideologies_widget.dart'
+    show SexualityandGenderIdeologiesWidget;
+export '/daily_activities/daily_activities_widget.dart'
+    show DailyActivitiesWidget;
+export '/responsibilityand_independence/responsibilityand_independence_widget.dart'
+    show ResponsibilityandIndependenceWidget;
+export '/creativityand_imagination/creativityand_imagination_widget.dart'
+    show CreativityandImaginationWidget;
+export '/gratitudeand_appreciation/gratitudeand_appreciation_widget.dart'
+    show GratitudeandAppreciationWidget;
+export '/group1/group1_widget.dart' show Group1Widget;
+export '/sign_in_copy/sign_in_copy_widget.dart' show SignInCopyWidget;
+export '/identity_self_worth_copy/identity_self_worth_copy_widget.dart'
+    show IdentitySelfWorthCopyWidget;
+export '/confidenceand_self_esteem/confidenceand_self_esteem_widget.dart'
+    show ConfidenceandSelfEsteemWidget;
+export '/audio_player_confidence/audio_player_confidence_widget.dart'
+    show AudioPlayerConfidenceWidget;
+export '/audio_player_academic/audio_player_academic_widget.dart'
+    show AudioPlayerAcademicWidget;
+export '/academic/academic_widget.dart' show AcademicWidget;
+export '/audio_player_sexuality/audio_player_sexuality_widget.dart'
+    show AudioPlayerSexualityWidget;
+export '/sexualit/sexualit_widget.dart' show SexualitWidget;
+export '/sexuality/sexuality_widget.dart' show SexualityWidget;
+export '/kids_profile_setup2/kids_profile_setup2_widget.dart'
+    show KidsProfileSetup2Widget;
+export '/kids_profile_setup3/kids_profile_setup3_widget.dart'
+    show KidsProfileSetup3Widget;
+export '/emotions/emotions_widget.dart' show EmotionsWidget;
+export '/audio_player_emotions/audio_player_emotions_widget.dart'
+    show AudioPlayerEmotionsWidget;
+export '/positivity/positivity_widget.dart' show PositivityWidget;
+export '/kindness/kindness_widget.dart' show KindnessWidget;
+export '/responsibility/responsibility_widget.dart' show ResponsibilityWidget;
+export '/respect/respect_widget.dart' show RespectWidget;
+export '/gratitude/gratitude_widget.dart' show GratitudeWidget;
+export '/audio_player_gratitude/audio_player_gratitude_widget.dart'
+    show AudioPlayerGratitudeWidget;
+export '/audio_player_respect/audio_player_respect_widget.dart'
+    show AudioPlayerRespectWidget;
+export '/audio_player_responsibility/audio_player_responsibility_widget.dart'
+    show AudioPlayerResponsibilityWidget;
+export '/audio_player_kindness/audio_player_kindness_widget.dart'
+    show AudioPlayerKindnessWidget;
+export '/audio_player_positivity/audio_player_positivity_widget.dart'
+    show AudioPlayerPositivityWidget;
+export '/audio_player_emotions_copy/audio_player_emotions_copy_widget.dart'
+    show AudioPlayerEmotionsCopyWidget;
+export '/creativity/creativity_widget.dart' show CreativityWidget;
+export '/audio_player_creativity/audio_player_creativity_widget.dart'
+    show AudioPlayerCreativityWidget;
+export '/alarm_identity/alarm_identity_widget.dart' show AlarmIdentityWidget;
+export '/alarm_confidence/alarm_confidence_widget.dart'
+    show AlarmConfidenceWidget;
+export '/alarm_emotion/alarm_emotion_widget.dart' show AlarmEmotionWidget;
+export '/alarm_positivity/alarm_positivity_widget.dart'
+    show AlarmPositivityWidget;
+export '/alarm_academics/alarm_academics_widget.dart' show AlarmAcademicsWidget;
+export '/alarm_sexuality/alarm_sexuality_widget.dart' show AlarmSexualityWidget;
+export '/alarm_kindness/alarm_kindness_widget.dart' show AlarmKindnessWidget;
+export '/alarm_responsibility/alarm_responsibility_widget.dart'
+    show AlarmResponsibilityWidget;
+export '/activities/activities_widget.dart' show ActivitiesWidget;
+export '/identity/identity_widget.dart' show IdentityWidget;
+export '/identity_player/identity_player_widget.dart' show IdentityPlayerWidget;
+export '/audio_test/audio_test_widget.dart' show AudioTestWidget;
+export '/riser_affirmation_copy/riser_affirmation_copy_widget.dart'
+    show RiserAffirmationCopyWidget;
+export '/activities_34/activities34_widget.dart' show Activities34Widget;
+export '/activities_68/activities68_widget.dart' show Activities68Widget;
+export '/activities_912/activities912_widget.dart' show Activities912Widget;
+export '/activities_teens/activities_teens_widget.dart'
+    show ActivitiesTeensWidget;
